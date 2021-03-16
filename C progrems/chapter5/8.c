a34f83ad019f59f4dbef7f2e2a51aac8fde73d7e
LiAuthor <193376161@qq.com>
2021-03-16 22:46:53 +0800

    add

---
+++ C progrems/chapter5/8.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+#include"math.h"
+int main(){
+    int bai,shi,ge;
+    for(int i=100;i<999;i++){
+        ge=i%10;
+        shi=(i/10)%10;
+        bai=i/100;
+        if(pow(ge,3)+pow(shi,3)+pow(bai,3)==i){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
